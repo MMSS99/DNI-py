1205144590c75858dbde7250042f4f3e5ab0b378
M. Marcelo S. Salgado <mmssalgado09@gmail.com>
2025-01-17 14:18:42 +0100

    refactor: :recycle: add class as object in dni constructor

--- src/dni.py
+++ src/dni.py
@@ -1,9 +1,9 @@
 from src.codigo_control import CodigoControl
-
 class Dni:
 
     def __init__(self, dni: str):
         self.dni = dni
+        self.codigoControl = CodigoControl()
 
 
     def setDni(self, dni: str):
@@ -19,7 +19,7 @@ class Dni:
         return self.dni[-1]
     
     def darLetra(self):
-        return self.dni + CodigoControl().asignarLetra(int(self.getDni()))
+        return self.dni + self.codigoControl.asignarLetra(int(self.getDni()))
 
     def comprobarDni(self):
-        return True if CodigoControl().comprobarLetra(self.getNumero(), self.getLetra()) else False
+        return True if self.codigoControl.comprobarLetra(self.getNumero(), self.getLetra()) else False
--- test/test_codigo_control.py
+++ test/test_codigo_control.py
@@ -2,11 +2,11 @@ import pytest
 from src.codigo_control import CodigoControl
 
 def test_asignarLetra():
-    assert CodigoControl().asignarLetra(3445523) == 'P'
-    assert CodigoControl().asignarLetra(91409311) == 'C'
-    assert CodigoControl().asignarLetra(32830547) == 'W'
+    assert CodigoControl.asignarLetra(3445523) == 'P'
+    assert CodigoControl.asignarLetra(91409311) == 'C'
+    assert CodigoControl.asignarLetra(32830547) == 'W'
 
 def test_comprobarLetra():
-    assert CodigoControl().comprobarLetra(3445523, 'P') == True
-    assert CodigoControl().comprobarLetra(91409311, 'C') == True
-    assert CodigoControl().comprobarLetra(32830547, 'C') == False
+    assert CodigoControl.comprobarLetra(3445523, 'P') == True
+    assert CodigoControl.comprobarLetra(91409311, 'C') == True
+    assert CodigoControl.comprobarLetra(32830547, 'C') == False
